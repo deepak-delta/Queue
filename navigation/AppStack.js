94c8980ba38e64e2471c703e11c78b8aa618fd4c
Deepak D krishna <ddk3857@gmail.com>
2021-05-07 13:49:45 +0530

    HomeScreen UI

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import HomeScreen from '../screens/HomeScreen';
 import Profile from '../screens/Profile';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
@@ -9,58 +10,78 @@ import auth from '@react-native-firebase/auth';
 import colors from '../utils/colors';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-const MyTabs = () => {
-  const [isSubmitted, setSbumitted] = useState();
+// const MyTabs = () => {
+//   const [isSubmitted, setSbumitted] = useState();
 
+//   return (
+//     <Tab.Navigator
+//       initialRouteName="Home"
+//       backBehavior="none"
+//       tabBarOptions={{
+//         activeTintColor: colors.icon,
+//         inactiveTintColor: colors.myColor,
+//         keyboardHidesTabBar: true,
+//         safeAreaInset: {bottom: 0, top: 'never'},
+//         style: {
+//           backgroundColor: colors.tab,
+//         },
+//       }}>
+//       <Tab.Screen
+//         name="Home"
+//         component={HomeScreen}
+//         options={{
+//           tabBarLabel: 'Home',
+
+//           tabBarIcon: ({color, size}) => (
+//             <MaterialCommunityIcons name="home" color={color} size={size} />
+//           ),
+//         }}
+//       />
+
+//       {/* <Tab.Screen
+//         name="Documents"
+//         component={DocNavigation}
+//         options={{
+//           tabBarLabel: 'Documents',
+//           tabBarIcon: ({ color, size }) => (
+//             <MaterialCommunityIcons name="file-document" color={color} size={size} />
+//           ),
+//         }}
+//       /> */}
+
+//       <Tab.Screen
+//         name="Profile"
+//         component={Profile}
+//         options={{
+//           tabBarLabel: 'Profile',
+//           tabBarIcon: ({color, size}) => (
+//             <MaterialCommunityIcons name="account" color={color} size={size} />
+//           ),
+//         }}
+//       />
+//     </Tab.Navigator>
+//   );
+// };
+
+// export default MyTabs;
+
+const AppStack = () => {
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      backBehavior="none"
-      tabBarOptions={{
-        activeTintColor: colors.icon,
-        inactiveTintColor: colors.myColor,
-        keyboardHidesTabBar: true,
-        safeAreaInset: {bottom: 0, top: 'never'},
-        style: {
-          backgroundColor: colors.tab,
-        },
-      }}>
-      <Tab.Screen
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
         name="Home"
         component={HomeScreen}
-        options={{
-          tabBarLabel: 'Home',
-
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
+        options={{header: () => null}}
       />
-
-      {/* <Tab.Screen
-        name="Documents"
-        component={DocNavigation}
-        options={{
-          tabBarLabel: 'Documents',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="file-document" color={color} size={size} />
-          ),
-        }}
-      /> */}
-
-      <Tab.Screen
+      <Stack.Screen
         name="Profile"
         component={Profile}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
+        options={{header: () => null}}
       />
-    </Tab.Navigator>
+    </Stack.Navigator>
   );
 };
 
-export default MyTabs;
+export default AppStack;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,40 +1,146 @@
-import React, {useEffect} from 'react';
-import {Text, View, StyleSheet, Dimensions, StatusBar} from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
 import colors from '../utils/colors';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const HomeScreen = () => {
-  const {height} = Dimensions.get('window');
-
-  useEffect(() => {
-    StatusBar.setBackgroundColor(colors.bg);
-  }, []);
-
+const HomeScreen = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <View style={{paddingTop: 50, paddingHorizontal: 14}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
-          <View>
-            <Text style={{fontSize: 26, color: colors.myColor}}>
-              Welcome Back,
-            </Text>
+    <ImageBackground
+      source={require('../assets/images/back.png')}
+      style={styles.image}>
+      <View style={styles.navbar}>
+        <MaterialCommunityIcons
+          name="menu"
+          size={30}
+          color="#a2a2db"
+          style={{width: 20}}
+        />
+        <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+          <MaterialCommunityIcons
+            name="account-circle"
+            size={33}
+            color="#a2a2db"
+            style={{marginLeft: 230}}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.container}>
+        <Text style={styles.heading}>Hello</Text>
+
+        <Text style={styles.subHeading}>
+          Lorem ipsum dolor sit amet, consectetuer adipscing elit.
+        </Text>
+
+        <View style={styles.buttonContainer}>
+          <View style={styles.btn}>
+            <MaterialCommunityIcons
+              name="office-building"
+              color="white"
+              size={32}
+            />
+          </View>
+
+          <View style={styles.btn}>
+            <MaterialCommunityIcons name="bus" color="white" size={32} />
+          </View>
+        </View>
+
+        <View style={styles.ticket}>
+          <Image
+            source={require('../assets/images/3.jpg')}
+            style={{
+              width: 100,
+              borderRadius: 100,
+              height: 100,
+            }}
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              width: 150,
+              alignItems: 'center',
+            }}>
+            <View
+              style={{
+                paddingHorizontal: 5,
+                paddingVertical: 5,
+              }}>
+              <Text
+                style={{
+                  fontFamily: 'RobotoRegular',
+                  fontSize: 11,
+                  color: '#a2a2db',
+                }}>
+                Lorem impsum dolor sit amet, consectetuer adipscing elit,
+              </Text>
+            </View>
           </View>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  navbar: {
+    flexDirection: 'row',
+    marginTop: 40,
+    alignItems: 'center',
+    paddingHorizontal: 40,
+  },
   container: {
-    flex: 1,
-    backgroundColor: colors.bg,
-    paddingTop: 0,
+    paddingHorizontal: 40,
+    marginTop: 25,
+  },
+  heading: {
+    fontSize: 40,
+    color: '#522289',
+    fontFamily: 'RobotoBold',
+  },
+  subHeading: {
+    fontSize: 15,
+    paddingVertical: 10,
+    paddingRight: 80,
+    lineHeight: 22,
+    fontFamily: 'RobotoRegular',
+    color: '#a2a2db',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    alignItems: 'center',
+    paddingHorizontal: 40,
+  },
+  btn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 66,
+    width: 66,
+    borderRadius: 50,
+    backgroundColor: '#ff5c83',
+    marginHorizontal: 22,
+  },
+  ticket: {
+    backgroundColor: '#ffffff',
+    height: 300,
+    width: 280,
+    borderRadius: 15,
+    padding: 5,
+    marginTop: 110,
   },
 });
